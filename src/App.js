c953b667c7343a9ed5cc686c7226057855bb04b8
GustavoTxFreitas <62919596+GustavoTxFreitas@users.noreply.github.com>
2020-08-25 15:46:01 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,25 @@ function App() {
   const [key, setKey] = useState('');
   const [text, setText] = useState('');
 
+  const createKeyword = () => {
+    var keyword = '', i = 0;
+    while(keyword.length < text.length){
+      if(i === key.length) i = 0;
+      
+      keyword += key[i];
+      i++
+    }
+
+    alert(keyword);
+  }
+
   return (
     <div className='App'>
       
       <Input label='key' value={key} setValue={setKey} />
       <Input label='text' value={text} setValue={setText} />
 
-      <button onClick={() => alert(`${key} ${text.length}`)}>
+      <button onClick={createKeyword}>
           Criar Keyword
       </button>
       
