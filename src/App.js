25e52a84038444d905f42f1a25593233b3dd0534
GustavoTxFreitas <62919596+GustavoTxFreitas@users.noreply.github.com>
2020-08-26 23:38:14 -0300

    react-vigenere-cipher v1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
+import { faKey, faFont } from '@fortawesome/free-solid-svg-icons';
+
 import Input from './components/Input/';
 import Button from './components/Button/';
 import './general.css'
 
-function App() {
+const App = () => {
   const [key, setKey] = useState('');
   const [text, setText] = useState('');
+  const display = document.querySelector('span#resultado');
 
   // Criar uma chave de criptografia
   const createKeyword = (keyword, length) => {
@@ -23,33 +26,55 @@ function App() {
 
     var newText = '';
     for (var i = 0; i < x; i++) {
+      if(!(text[i] >= 'A' && text[i] <= 'Z')){
+        newText += text[i];
+        continue;
+      }
       newText += String.fromCharCode((text.charCodeAt(i) + keyword.charCodeAt(i)) % 26 + 65);
     }
-    alert(newText);
+    display.innerText = newText;
   }
 
   // Descriptografar o texto a partir da chave
-  const originText = () => {
+  function originText(){
     var x = text.length;
     var keyword = createKeyword('', x);
 
     var newText = '';
     for (var i = 0; i < x; i++) {
+      if(!(text[i] >= 'A' && text[i] <= 'Z')){
+        newText += text[i];
+        continue;
+      }
       newText += String.fromCharCode((text.charCodeAt(i) - keyword.charCodeAt(i) + 26) % 26 + 65);
     }
-    alert(newText);
+    display.innerText = newText;
   }
 
   return (
     <div id='App'>
-      <Input label='key' value={key} setValue={setKey} />
-      <Input label='text' value={text} setValue={setText} />
-
-      <div id='buttonContainer'>
-        <Button event={cipherText} text='Criptografar' />
-        <Button event={originText} text='Decifrar' />
+      <div id='column1'>
+        <p>Antes:</p>
+        <span className='display'>{text || 'Valor'}</span>
+        <p>Depois:</p>
+        <span id='resultado' className='display'>Valor</span>
       </div>
 
+      <div id='column2'>
+
+        <Input label='Insira uma Chave' icon={faKey} value={key} setValue={setKey} />
+
+        <Input label='Insira um Texto' icon={faFont} value={text} setValue={setText} />
+
+        <div id='buttonContainer'>
+
+          <Button event={cipherText} text='Criptografar' />
+
+          <Button event={originText} text='Decifrar' />
+
+        </div>
+
+      </div>
     </div>
   );
 }
--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import './style.css';
 
-function Input({ label, value, setValue }) {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+function Input({ label, icon, value, setValue }) {
+    const iconElement = <FontAwesomeIcon className='icon' icon={icon} />
     
     return (
         <div>
+            <p className='label'>{label}:</p>
             <label className='inputContainer'>
-                <span className='inputLabel'>{label}:</span>
-                <input className='input' type='text' 
-                value={value}
-                onChange={e => setValue(e.target.value.toUpperCase())}
-                placeholder='Digite algo:' />
-                
+                {iconElement}
+                <input className='input' type='text'
+                    value={value}
+                    maxLength='50'
+                    autoFocus
+                    onChange={e => setValue(e.target.value.toUpperCase())}
+                    placeholder='Digite algo:' />
             </label>
         </div>
     );
